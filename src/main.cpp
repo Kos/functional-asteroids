3f2d52c407877c291c47e9d53d56b99c45e527e7
Tomasz Wesolowski <kosashi@gmail.com>
2014-08-03 12:36:14 +0200

    'Scaffolding'

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,47 @@
 #include <iostream>
+#include <vector>
+#include "GL/gl.h"
 #include "foo.h"
 
 using std::cout;
 using std::endl;
-int main() {
-	cout << "Hello there! " << x << endl;
+using std::vector;
+
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClearColor(0.,0.,1.,0.);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
