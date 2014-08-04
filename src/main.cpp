6c0a68a84709243268077be41900347aa06d51ed
Tomasz Wesolowski <kosashi@gmail.com>
2014-08-04 22:51:10 +0200

    'Cleanups'

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <list>
 #include <functional>
+#include <random>
 #include "GL/gl.h"
 #include "GL/wglext.h"
 #include "GL/glu.h"
@@ -35,27 +36,57 @@ struct vec2 {
 	float y;
 };
 
+std::mt19937 rng;
 
-std::list<std::function<void(int, int, int, int)>> on_key;
-void keyfun(GLFWwindow * window, int key, int scancode, int action, int mods) {
-	/*
-	 * [in]	window	The window that received the event.
-[in]	key	The keyboard key that was pressed or released.
-[in]	scancode	The system-specific scancode of the key.
-[in]	action	GLFW_PRESS, GLFW_RELEASE or GLFW_REPEAT.
-[in]	mods	Bit field describing which modifier keys were held down
-	 */
-	for (auto x : on_key) {
-		x(key, scancode, action, mods);
-	}
+typedef unsigned long long guid;
+guid random_guid() {
+	return std::uniform_int_distribution<guid>()(rng);
 }
 
-void bind_key(int key, std::function<void()> callback) {
-	on_key.push_back([=](int key2, int scancode, int action, int mods){
-		if (key2 == key and action == GLFW_PRESS) {
-			callback();
+struct Object {
+	Object(vec2 pos, vec2 speed) : pos(pos), speed(speed) {}
+	long guid = random_guid();
+	vec2 pos;
+	vec2 speed;
+};
+
+struct World {
+	vector<Object> objects;
+	list<function<void()>> queue;
+	list<function<void(int, int, int, int)>> key_events;
+
+	void tick(float dt) {
+		for (auto& o : objects) {
+			o.pos.x += o.speed.x * dt;
+			o.pos.y += o.speed.y * dt;
 		}
-	});
+	}
+
+	void keyfun(int key, int scancode, int action, int mods) {
+		/*
+		 * [in]	window	The window that received the event.
+		 * [in]	key	The keyboard key that was pressed or released.
+		 * [in]	scancode	The system-specific scancode of the key.
+		 * [in]	action	GLFW_PRESS, GLFW_RELEASE or GLFW_REPEAT.
+		 * [in]	mods	Bit field describing which modifier keys were held down
+		 */
+		for (auto x : key_events) {
+			x(key, scancode, action, mods);
+		}
+	}
+
+	void bind_key(int key, std::function<void()> callback) {
+		key_events.push_back([=](int key2, int scancode, int action, int mods){
+			if (key2 == key and action == GLFW_PRESS) {
+				callback();
+			}
+		});
+	}
+};
+
+World* cb_world;
+void cb_key(GLFWwindow*, int key, int scancode, int action, int mods) {
+	cb_world->keyfun(key, scancode, action, mods);
 }
 
 
@@ -63,39 +94,34 @@ int SWAP_INTERVAL = 1;
 
 int main(void)
 {
-	GLFWwindow* window;
-
-	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
 	glfwWindowHint(GLFW_SAMPLES, 4);
 
-	/* Create a windowed mode window and its OpenGL context */
-
-	window = glfwCreateWindow(1366, 768, "Hello World", glfwGetPrimaryMonitor(), NULL);
+	GLFWwindow* window;
+	// window = glfwCreateWindow(1366, 768, "Hello World", glfwGetPrimaryMonitor(), 0);
+	window = glfwCreateWindow(800, 480, "Hello World", 0, 0);
 
 	if (!window)
 	{
-		cout << "Could not create window" << endl;
+		cerr << "Could not create window" << endl;
 		glfwTerminate();
-		return -1;
+		exit(-1);
 	}
-	glfwSetKeyCallback(window, keyfun);
 
-	vector<vec2> objs = {
-			{0, 0},
-			{1, 1}
-	};
-	vector<vec2> speeds = {
-			{0.2, 0},
-			{0.2, 0.2}
+	World world;
+
+	cb_world = &world;
+	glfwSetKeyCallback(window, cb_key);
+
+	world.objects = {
+		Object(vec2{.2, .2}, vec2{0.2, 0}),
+		Object(vec2{1, 1}, vec2{-0.2, -0.2})
 	};
 
-	bind_key(GLFW_KEY_SPACE, [&](){
-		speeds[0].x *= -1;
+	world.bind_key(GLFW_KEY_SPACE, [&](){
+		world.objects[0].speed.x *= -1;
 	});
-
-	list<function<void()>> queue;
 
 
 
@@ -108,12 +134,10 @@ int main(void)
 
 	PFNWGLSWAPINTERVALEXTPROC wglSwapIntervalEXT = (PFNWGLSWAPINTERVALEXTPROC)glfwGetProcAddress("wglSwapIntervalEXT");
 	if (wglSwapIntervalEXT) {
-		cerr << "VSync not available" << endl;
-	} else {
 		wglSwapIntervalEXT(1);
+	} else {
+		cerr << "VSync not available" << endl;
 	}
-
-
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -126,10 +150,9 @@ int main(void)
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glColor3f(.5,.2,0);
-		for (int i=0; i<objs.size(); ++i) {
-			objs[i].x += speeds[i].x*dt;
-			objs[i].y += speeds[i].y*dt;
-			ding(objs[i].x, objs[i].y);
+		world.tick(dt);
+		for (auto o : world.objects) {
+			ding(o.pos.x, o.pos.y);
 		}
 
 		/* Swap front and back buffers */
@@ -137,9 +160,11 @@ int main(void)
 
 		/* Poll for and process events */
 		glfwPollEvents();
+
 	}
 
 	glfwTerminate();
 	return 0;
 }
+
 
