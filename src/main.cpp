2ef6e494057a8db46fa62700b184feae8809d459
Tomasz Wesolowski <kosashi@gmail.com>
2014-08-04 23:29:48 +0200

    Objects and object state

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <list>
 #include <functional>
 #include <random>
+#include <memory>
 #include "GL/gl.h"
 #include "GL/wglext.h"
 #include "GL/glu.h"
@@ -44,6 +45,7 @@ guid random_guid() {
 }
 
 struct Object {
+	Object() {}
 	Object(vec2 pos, vec2 speed) : pos(pos), speed(speed) {}
 	long guid = random_guid();
 	vec2 pos;
@@ -51,7 +53,8 @@ struct Object {
 };
 
 struct World {
-	vector<Object> objects;
+	list<Object> objects; // could be a vector, but reallocs will kill references in callbacks
+	list<function<void()>> tick_events;
 	list<function<void()>> queue;
 	list<function<void(int, int, int, int)>> key_events;
 
@@ -60,6 +63,23 @@ struct World {
 			o.pos.x += o.speed.x * dt;
 			o.pos.y += o.speed.y * dt;
 		}
+		for (auto t : tick_events) {
+			t();
+		}
+	}
+
+	Object* find_by_guid(guid g) {
+		for (auto& o : objects) {
+			if (o.guid == g) {
+				return &o;
+			}
+		}
+		return 0;
+	}
+
+	Object& add_object() {
+		objects.push_back(Object());
+		return objects.back();
 	}
 
 	void keyfun(int key, int scancode, int action, int mods) {
@@ -82,15 +102,77 @@ struct World {
 			}
 		});
 	}
+
+	void on_tick(std::function<void()> callback) {
+		tick_events.push_back(callback);
+	}
 };
 
+GLFWwindow* window;
 World* cb_world;
 void cb_key(GLFWwindow*, int key, int scancode, int action, int mods) {
 	cb_world->keyfun(key, scancode, action, mods);
 }
 
 
-int SWAP_INTERVAL = 1;
+
+void WrapScreen(World& w, Object& o) {
+	w.tick_events.push_back([&o](){
+		float m = 0.1;
+		float xa = -1-m, xb = 1+m, ya=-1-m, yb=1+m;
+		float xw=xb-xa, yw=yb-ya;
+		if (o.pos.x < xa) {
+			o.pos.x += xw;
+		}
+		if (o.pos.x > xb) {
+			o.pos.x -= xw;
+		}
+		if (o.pos.y < ya) {
+			o.pos.y += yw;
+		}
+		if (o.pos.y > yb) {
+			o.pos.y -= yw;
+		}
+	});
+}
+
+void Asteroid(World& w, Object& o) {
+	o.pos.x = std::uniform_real_distribution<float>(-1, 1)(rng);
+	o.pos.y = std::uniform_real_distribution<float>(-1, 1)(rng);
+	float speed = 0.3;
+	float angle = std::uniform_real_distribution<float>(0, 44./7)(rng);
+	o.speed.x = cos(angle)*speed;
+	o.speed.y = sin(angle)*speed;
+	WrapScreen(w, o);
+}
+
+
+void Player(World& w, Object& o) {
+	Asteroid(w, o);
+	std::shared_ptr<float> angle(new float);
+
+	w.on_tick([&o, angle](){
+		if (glfwGetKey(window, GLFW_KEY_RIGHT)) {
+			*angle += 0.1;
+		}
+		if (glfwGetKey(window, GLFW_KEY_LEFT)) {
+			*angle -= 0.1;
+		}
+		if (glfwGetKey(window, GLFW_KEY_UP)) {
+			float acc = 0.1;
+			cout << *angle;
+			o.speed.x += cos(*angle)*acc;
+			o.speed.y += sin(*angle)*acc;
+		}
+	});
+
+	w.bind_key(GLFW_KEY_SPACE, [&o](){
+		float speed = 0.3;
+		float angle = std::uniform_real_distribution<float>(0, 44./7)(rng);
+		o.speed.x = cos(angle)*speed;
+		o.speed.y = sin(angle)*speed;
+	});
+}
 
 int main(void)
 {
@@ -98,7 +180,6 @@ int main(void)
 		return -1;
 	glfwWindowHint(GLFW_SAMPLES, 4);
 
-	GLFWwindow* window;
 	// window = glfwCreateWindow(1366, 768, "Hello World", glfwGetPrimaryMonitor(), 0);
 	window = glfwCreateWindow(800, 480, "Hello World", 0, 0);
 
@@ -110,19 +191,13 @@ int main(void)
 	}
 
 	World world;
-
 	cb_world = &world;
 	glfwSetKeyCallback(window, cb_key);
 
-	world.objects = {
-		Object(vec2{.2, .2}, vec2{0.2, 0}),
-		Object(vec2{1, 1}, vec2{-0.2, -0.2})
-	};
-
-	world.bind_key(GLFW_KEY_SPACE, [&](){
-		world.objects[0].speed.x *= -1;
-	});
-
+	for (int i=0; i<5; ++i) {
+		Asteroid(world, world.add_object());
+	}
+	Player(world, world.add_object());
 
 
 	/* Make the window's context current */
